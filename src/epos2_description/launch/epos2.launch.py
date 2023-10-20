3d0fa1a58057dcf13704962f3b1ff0eb1e01a3bd
s22655429s <s22655429s@gmail.com>
2023-10-20 17:14:21 +0800

    can control and rotate

--- src/epos2_description/launch/epos2.launch.py
+++ src/epos2_description/launch/epos2.launch.py
@@ -76,12 +76,12 @@ def generate_launch_description():
             output="screen",
         ),
 
-        # Node(
-        #     package="rviz2",
-        #     executable="rviz2",
-        #     name="rviz2",
-        #     arguments=["-d", rviz_config],
-        #     output="screen",
-        # )
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=["-d", rviz_config],
+            output="screen",
+        )
 
     ])
